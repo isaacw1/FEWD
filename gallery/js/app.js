fe139baf66326e6619afb93579c3144edf08dff0
isaacw1 <idubb818@aol.com>
2015-07-01 17:02:12 -0700

    everything

---
+++ gallery/js/app.js
@@ -0,0 +1,9 @@
+var sliderElement = document.getElementById('slider');
+sliderElement.Slider();
+
+var navElement = document.getElementById('nav');
+navElement.Nav();
+navElement.showSection('contact');
+
+var galleryElement = document.getElementById('gallery');
+galleryElement.Gallery();
