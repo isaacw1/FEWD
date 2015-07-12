51426d4e87556166c6e24c82b67172911aaf37e1
isaacw1 <idubb818@aol.com>
2015-07-12 15:26:59 -0700

    card work

--- card-match/html/js/app.js
+++ card-match/html/js/app.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+
+$("#test1").append("<div class='front'><'img src="img/bicycle-back.jpg"'/></div>");
+
+});
+
+
 (function() {
   var cardflip = document.querySelectorAll(".card.effect__click");
   for ( var i  = 0, len = cardflip.length; i < len; i++ ) {
